b52e367e6b506b12013ca138737a9db7e6d625dc
MicJoz <116846798+MicJoz@users.noreply.github.com>
2022-12-07 01:15:01 +0100

    Function to reverse string given by user

---
+++ string_to_reverse.py
@@ -0,0 +1,12 @@
+def string_to_reverse(str_to_reverse):
+    reversed_string = str_to_reverse[::-1]
+    return reversed_string
+
+
+str_to_reverse = input("Enter a string to reverse: ")
+print(f"Reversed string = {string_to_reverse(str_to_reverse)}")
+
+
+
+
+
