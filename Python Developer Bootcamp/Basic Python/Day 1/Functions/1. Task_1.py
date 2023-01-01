c9f78fc48e9edf042483b319c9ec473c49d38841
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-01 18:55:42 +0100

    Task 1 - done

---
+++ Python Developer Bootcamp/Basic Python/Day 1/Functions/1. Task_1.py
@@ -0,0 +1,14 @@
+def square(num):
+    return num * num
+
+
+def square_print(num):
+    print(num * num)
+
+
+if __name__ == "__main__":
+    a = square(10) + 10
+    print(a)
+
+    b = square_print(10) + 10
+    print(b)
