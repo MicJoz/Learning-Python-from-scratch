e13aa0953f17082421409469d1ed5e5725868db5
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-01 18:32:57 +0100

    Added new functions

--- Python Developer Bootcamp/Basic Python/Workshop/1. guess_a_number.py
+++ Python Developer Bootcamp/Basic Python/Workshop/1. guess_a_number.py
@@ -1,24 +1,33 @@
 import random
 
-number_to_guess = random.randint(1, 100)
 
-while True:
-    try:
-        player_number = int(input("Guess the number: "))
-        break
-    except ValueError:
-        print("It's not a number!")
+def getting_number():
+    while True:
+        try:
+            player_number = int(input("Guess the number: "))
+            break
+        except ValueError:
+            print("It's not a number!")
+    return player_number
 
-is_guessed = False
 
-while not is_guessed:
-    if player_number < number_to_guess:
-        print("Too small!")
-        player_number = int(input("Guess the number: "))
-    elif player_number > number_to_guess:
-        print("Too big!")
-        player_number = int(input("Guess the number: "))
-    else:
-        print("You win!")
-        is_guessed = True
+def guessing_number():
+    number_to_guess = random.randint(1, 100)
+    player_number = getting_number()
 
+    is_guessed = False
+
+    while not is_guessed:
+        if player_number < number_to_guess:
+            print("Too small!")
+            player_number = int(input("Guess the number: "))
+        elif player_number > number_to_guess:
+            print("Too big!")
+            player_number = int(input("Guess the number: "))
+        else:
+            print("You win!")
+            is_guessed = True
+
+
+if __name__ == '__main__':
+    guessing_number()
