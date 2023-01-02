27f5dbf641a602439ba5a9b6f390773a975fd26b
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-02 22:26:39 +0100

    Modified for loop in def histogram

--- Python Developer Bootcamp/Basic Python/Day 1/Homework/7.histogram.py
+++ Python Developer Bootcamp/Basic Python/Day 1/Homework/7.histogram.py
@@ -1,19 +1,20 @@
 def histogram(input_list):
-    display = ""
-    for item in input_list:
-        if type(item) != int:
+    for i in input_list:
+        if type(i) != int:
             return None
     else:
-        for i in range(len(input_list)):
-            if display != "":
-                display += "\n"
-            for j in range(input_list[i]):
+        display = ""
+        for i in input_list:
+            # if display != "": ## adds blank line between h and h1 print
+            display += "\n"
+            for j in range(i):
                 display += "#"
     return display
 
 
-h = histogram([1, 2, 3, "r"])
-print(h)
-h1 = histogram([4, 3, 5, 2, 1])
-print(h1)
+if __name__ == "__main__":
+    h = histogram([1, 2, 3, "r"])
+    print(h)
+    h1 = histogram([4, 3, 5, 2, 1])
+    print(h1)
 
