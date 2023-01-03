902e3298258dce2f73cd82d6134ad3b5c17dbfda
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-03 22:11:01 +0100

    Task 9.1 - done

---
+++ Real Python - Basic Python/9.1.review_exercises.py
@@ -0,0 +1,10 @@
+cardinal_numbers = ("first", "second", "third")
+print(cardinal_numbers[1])
+position1, position2, position3 = cardinal_numbers
+print(position1)
+print(position2)
+print(position3)
+my_name = tuple("Michu")
+print("x" in my_name)
+new_name = my_name[1:]
+print(new_name)
