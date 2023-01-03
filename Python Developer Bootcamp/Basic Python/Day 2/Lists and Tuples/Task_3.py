df4f2c23558669352f89a94d3b55c780755e5dfe
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-03 23:25:38 +0100

    List comprehension added

---
+++ Python Developer Bootcamp/Basic Python/Day 2/Lists and Tuples/Task_3.py
@@ -0,0 +1,11 @@
+def find_short_words(input_list):
+    # short_word_list = []
+    # for i in input_list:
+    #     if len(i) < 5:
+    #         short_word_list.append(i)
+    short_word_list = [i for i in input_list if len(i) < 5]  # added list comprehension
+    return short_word_list
+
+
+if __name__ == "__main__":
+    print(find_short_words(['Litwo', 'ojczyzno', 'moja', 'ty', 'jesteś', 'jak', 'zdrowie']))
