f004905e0eb6c9e3064dfe2b59ed8c324049e7f8
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-04 23:28:41 +0100

    Task 9.6 - done

---
+++ Real Python - Basic Python/9.6.review_exercises.py
@@ -0,0 +1,17 @@
+captains = {}
+captains["Enterprise"] = "Picard"
+captains["Voyager"] = "Janeway"
+captains["Defiant"] = "Sisko"
+
+if "Enterprise" not in captains:
+    captains["Enterprise"] = "unknown"
+
+if "Discovery" not in captains:
+    captains["Discovery"] = "unknown"
+
+for ship, captain in captains.items():
+    print(f"The {ship} is captained by {captain}")
+
+del captains["Discovery"]
+
+captains_copy = dict((('Enterprise', 'Picard'), ('Voyager', 'Janeway'), ('Defiant', 'Sisko')))
