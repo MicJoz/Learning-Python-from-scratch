ec394253c6817860cb20b139d4740f8e3de4dcf9
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-06 17:08:08 +0100

    List comprehension added

--- Python Developer Bootcamp/Basic Python/PreTest/4. div.py
+++ Python Developer Bootcamp/Basic Python/PreTest/4. div.py
@@ -1,12 +1,13 @@
 def div(min, max):
-    min = int(input("Enter a position to start: "))
-    max = int(input("Enter a position to stop: "))
-    div_list = []
-    for i in range(min, max + 1):
-        if i % 2 == 0 and not i % 3 == 0:
-            div_list.append(i)
+    # div_list = []
+    # for i in range(min, max + 1):
+    #    if i % 2 == 0 and not i % 3 == 0:
+    #        div_list.append(i)
+    div_list = [i for i in range(min, max + 1) if i % 2 == 0 and not i % 3 == 0]  # list comprehension added
     return div_list
 
 
 if __name__ == '__main__':
+    min = int(input("Enter a position to start: "))
+    max = int(input("Enter a position to stop: "))
     print(div(min, max))
