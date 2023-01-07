ab1f13f127cc980655f06b4352d7fe8cfe785875
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-07 01:29:17 +0100

    Task 5 - done

---
+++ Python Developer Bootcamp/Basic Python/Day 2/Modules/Task_5.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.insert(1, "./math_tools")
+
+from Functions import quadratic_function
+
+
+if __name__ == "__main__":
+    print(quadratic_function(2, 9, 4, 6))
---
+++ Python Developer Bootcamp/Basic Python/Day 2/Modules/math_tools/Task_5.py
@@ -0,0 +1,4 @@
+from Functions import quadratic_function
+
+if __name__ == "__main__":
+    print(quadratic_function(2, 9, 4, 6))
