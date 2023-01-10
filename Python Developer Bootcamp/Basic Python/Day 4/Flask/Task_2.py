4854e9c848c79f302c783659dcce843a02021f44
MichalJozefiak <mi.jozefiak@gmail.com>
2023-01-10 15:43:37 +0100

    Time function ammended according to Task 2

--- Python Developer Bootcamp/Basic Python/Day 4/Flask/Task_2.py
+++ Python Developer Bootcamp/Basic Python/Day 4/Flask/Task_2.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 
 @app.route("/<string:current_time>")
-def data(current_time):
-    current_time = datetime.now()
-    time = current_time.strftime("%d %B %Y %H:%M:%S")
-    return f"{time}"
+def time(current_time):
+    current_time = (datetime.now()).strftime("%H:%M:%S")
+    return f"{current_time}"
 
 
 if __name__ == "__main__":
